c689f9855a241cb77c51375f537054af02b615ed
lhnpython <101509265+lhnpython@users.noreply.github.com>
2022-04-01 10:10:28 +0800

    Update photoscut.py

--- tool/photoscut.py
+++ tool/photoscut.py
@@ -7,10 +7,10 @@ xia = int(input('请输入下剪裁像素: '))
 zuo = int(input('请输入左剪裁像素: '))
 you = int(input('请输入右剪裁像素: '))
 print('将把图片剪裁为: 横向'+str(zuo)+'~'+str(you)+' 纵向'+str(shang)+'~'+str(xia))
-filesnames = os.listdir('.')
+filesnames = listdir('.')
 for name in filesnames:
     try:
-        img = Image.open(name)
+        img = open(name)
         cropped = img.crop((zuo, shang, you, xia))
         cropped.save("cut-{}".format(name))
     except:
